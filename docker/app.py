c248daacb95b109fdecd256cb9e6260adb2f853a
Samuel Esquivel Abasolo <AL03000956@tecmilenio.mx>
2026-04-09 09:59:35 -0600

    Revert "Feat/docker web app"

--- docker/app.py
+++
@@ -1,9 +0,0 @@
-#Página web con flask
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "¡Hola! Bienevenido a la app en docker"
-
-app.run(host='0.0.0.0', port=5000)
